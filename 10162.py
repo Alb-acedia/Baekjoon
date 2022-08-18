f9ce65911d5ad35822cc022cb0eea302b73a3eff
Alb-acedia <49442998+lazyBsloth@users.noreply.github.com>
2022-08-19 08:47:34 +0900

    220819 python

---
+++ 10162.py
@@ -0,0 +1,26 @@
+t = int(input())
+a, b, c = 0, 0, 0
+
+while t > 0:
+    t -= 300
+    a += 1
+if t < 0:
+    t += 300
+    a -= 1
+while t > 0:
+    t -= 60
+    b += 1
+if t < 0:
+    t += 60
+    b -= 1
+while t > 0:
+    t -= 10
+    c += 1
+if t < 0:
+    t += 10
+    c -= 1
+
+if t == 0:
+    print(a, b, c)
+else:
+    print(-1)
