6a992c37b686e7be2b98d0b36abf0c414bcced1f
Alb-acedia <49442998+lazyBsloth@users.noreply.github.com>
2022-08-30 09:34:07 +0900

    220830

---
+++ 10214.py
@@ -0,0 +1,13 @@
+t = int(input())
+for _ in range(t):
+    Y = K = 0
+    for _ in range(9):
+        y, k = map(int, input().split())
+        Y += y
+        K += k
+    if Y > K:
+        print("Yonsei")
+    elif Y < K:
+        print("Korea")
+    else:
+        print("Draw")
