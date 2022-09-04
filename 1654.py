bd15cc1195cd0e2713de7ae19de761a776b86cf7
Alb-acedia <49442998+lazyBsloth@users.noreply.github.com>
2022-09-04 09:58:27 +0900

    220904

---
+++ 1654.py
@@ -0,0 +1,23 @@
+# 이분탐색
+import sys
+
+lan = []
+total = 0
+k, n = map(int, input().split())
+for _ in range(k):
+    lan.append(int(sys.stdin.readline()))
+
+st, end = 1, max(lan)
+
+while st <= end:
+    mid = (st + end) // 2
+    cnt = 0
+    for i in lan:
+        cnt += i // mid
+
+    if cnt >= n:
+        st = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
