9cde988212c8f13b9ecd4939ed06a7975f2366ea
Alb-acedia <49442998+lazyBsloth@users.noreply.github.com>
2022-09-12 00:10:29 +0900

    1978.py

--- 1978.py
+++ 1978.py
@@ -2,12 +2,14 @@ n = int(input())
 result = 0
 num_list = list(map(int, input().split()))
 for i in num_list:
-    isOrder = False
+    isPrim = True
+    if i == 1:
+        isPrim = False
     for x in range(2, i):
         if i % x == 0:
-            isOrder = True
+            isPrim = False
 
-    if not isOrder:
+    if isPrim:
         result += 1
 
 print(result)
