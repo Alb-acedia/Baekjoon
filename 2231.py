2e47a2f26d717f13574bffe157b9ee773986ab38
Alb-acedia <49442998+lazyBsloth@users.noreply.github.com>
2022-09-18 18:04:53 +0900

    220918

--- 2231.py
+++ 2231.py
@@ -1,6 +1,11 @@
 n = int(input())
 result = 0
 
-for i in range(200, n + 1):
+for i in range(1, n + 1):
     m = list(map(int, str(i)))
-    print(m)
+    result = i + sum(m)
+    if result == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
